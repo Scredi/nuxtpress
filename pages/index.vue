2dc882ca4dd50586faf4bfcf69413bcf844d0ea2
Maxime Petronio <m.petronio@wami.lan>
2018-03-06 16:41:59 +0100

    First commit

--- pages/index.vue
+++ pages/index.vue
@@ -3,27 +3,27 @@
 </template>
 
 <script>
-import Posts from '~/components/Posts.vue'
-import { getPaginatedPosts } from '~/api/api'
-export default {
-  components: { Posts },
-  head () {
-    return {
-      title: `Accueil`,
-      meta: [
-        {
-          hid: 'description',
-          name: 'description',
-          content: 'SEO content accueil'
-        }
-      ]
-    }
-  },
-  async asyncData ({params}) {
-    const posts = await getPaginatedPosts(10, params.query ? params.query.page : 1)
-    return {
-      posts
+  import Posts from '~/components/Posts.vue'
+  import { getPaginatedPosts } from '~/api/api'
+  export default {
+    components: { Posts },
+    head () {
+      return {
+        title: `Accueil`,
+        meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            content: 'SEO content accueil'
+          }
+        ]
+      }
+    },
+    async asyncData ({params}) {
+      const posts = await getPaginatedPosts(10, params.query ? params.query.page : 1)
+      return {
+        posts
+      }
     }
   }
-}
 </script>
