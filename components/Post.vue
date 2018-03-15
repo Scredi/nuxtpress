26d21efc15e5755e638b621ee2655062d5b3c8b0
Maxime Petronio <m.petronio@wami.lan>
2018-03-15 17:23:03 +0100

    css update

--- components/Post.vue
+++ components/Post.vue
@@ -43,4 +43,64 @@
             width: 100%;
         }
     }
+    ul {
+        list-style: none;
+    }
+    pre {
+        position: relative;
+        line-height: 28px;
+        background: rgba(0, 0, 0, .85);
+        color: white;
+
+        &::before {
+            color: white;
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+        }
+
+        &[rel="Javascript"]::before, &[rel="jQuery"]::before, &[rel="JSX"]::before {
+            content: "JS";
+            background: #9c27b0;
+        }
+
+        &[rel="PHP"]::before {
+            content: "PHP";
+            background: #999;
+        }
+
+        &[rel="CSS"]::before {
+            content: "CSS";
+            background: #ff9800;
+        }
+        
+        &[rel="SCSS"]::before {
+            content: "SCSS";
+            background: #999;
+        }
+
+        &[rel="SVG"]::before {
+            content: "SVG";
+            background: #999;
+        }
+
+        &[rel="HTML"]::before {
+            content: "HTML";
+            background: #4caf50;
+        }
+
+        &[rel="HTAccess"]::before {
+            content: "HTAccess";
+            background: #999;
+        }
+
+        code {
+            white-space: pre;
+            padding: 2rem 1.5rem;
+            line-height: 1.75;
+            display: block;
+            overflow-x: scroll;
+        }
+    }
 </style>
